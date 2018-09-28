8595fdb8004d86e507e87b885922c2fa3abb1a6e
mordini <yahewisthedevil@yahoo.com>
2018-09-28 07:41:45 -0400

    Section 5 - Lecture 68. More on Viewcapsulation (Overriding them)

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+
+  // Overriding ViewEncapsulation
+  encapsulation: ViewEncapsulation.Emulated // None, Native
 })
 export class ServerElementComponent implements OnInit {
   
